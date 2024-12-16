89417ac95e9708f864811569bc80221467e8ef9d
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-12-16 14:05:03 +0700

    membuat fun Mahasiswa di UpdateMhsViewModel.kt

--- app/src/main/java/com/example/meet09/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/meet09/ui/viewmodel/UpdateMhsViewModel.kt
@@ -2,6 +2,7 @@ package com.example.meet09.ui.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.example.meet09.data.entity.Mahasiswa
 import com.example.meet09.repository.RepositoryMhs
 
 class UpdateMhsViewModel(
@@ -9,4 +10,9 @@ class UpdateMhsViewModel(
     private val repositoryMhs: RepositoryMhs
 ) : ViewModel() {
 
+
 }
+
+fun Mahasiswa.toUIStateMhs() : MhsUIState = MhsUIState (
+    mahasiswaEvent = this.toDetailUiEvent(),
+)

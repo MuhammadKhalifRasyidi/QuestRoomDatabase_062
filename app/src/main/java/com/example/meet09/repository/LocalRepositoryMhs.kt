c86da72fc66216bf14f450e768a2e60c8dc0400b
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-12-16 15:19:54 +0700

    menambahkan kode dalam File LocalRepositoryMhs.kt

--- app/src/main/java/com/example/meet09/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/meet09/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,26 @@ package com.example.meet09.repository
 
 import com.example.meet09.data.dao.MahasiswaDao
 import com.example.meet09.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs {
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> = mahasiswaDao.getAllMahasiswa()
+
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> = mahasiswaDao.getMahasiswa(nim)
+
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }

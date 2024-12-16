2cfd65cc9091e59d52a7b1fcb3eef42bd5d4dfbc
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-12-16 12:49:59 +0700

    menambahkan class HomeMhsViewModel

--- app/src/main/java/com/example/meet09/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/meet09/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,7 +1,12 @@
 package com.example.meet09.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
 import com.example.meet09.data.entity.Mahasiswa
+import com.example.meet09.repository.RepositoryMhs
 
+class HomeMhsViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {}
 
 data class HomeUiState (
     val listMhs: List<Mahasiswa> = listOf(),

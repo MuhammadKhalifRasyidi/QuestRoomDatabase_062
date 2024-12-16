e44bf87c74e983ab122e387a0a4d6997f8781c77
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-12-16 12:47:48 +0700

    menambahkan data class HomeUiState

--- app/src/main/java/com/example/meet09/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/meet09/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,1 +1,11 @@
 package com.example.meet09.ui.viewmodel
+
+import com.example.meet09.data.entity.Mahasiswa
+
+
+data class HomeUiState (
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)

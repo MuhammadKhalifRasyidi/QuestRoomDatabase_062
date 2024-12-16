19e38ce26d6434a5867f5812d8bf40d1946941ef
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-12-16 14:01:20 +0700

    membuat class UpdateMhsViewModel di UpdateMhsViewModel.kt

--- app/src/main/java/com/example/meet09/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/meet09/ui/viewmodel/UpdateMhsViewModel.kt
@@ -1,2 +1,12 @@
 package com.example.meet09.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.meet09.repository.RepositoryMhs
+
+class UpdateMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+}

948cb86402270506b1cfccdca6b332ff801bbf6c
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-12-16 15:12:31 +0700

    menambahkan kode dalam File MahasiswaDao.kt

--- app/src/main/java/com/example/meet09/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/meet09/data/dao/MahasiswaDao.kt
@@ -1,13 +1,27 @@
 package com.example.meet09.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.meet09.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
     @Insert
-    suspend fun insertMahasiswa(
-        mahasiswa: Mahasiswa
-    )
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
 }

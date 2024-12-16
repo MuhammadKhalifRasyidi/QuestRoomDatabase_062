2fc27e57768902b750df56471a26ffeea7bd2953
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-12-16 15:37:59 +0700

    merapihkan isi file

--- app/src/main/java/com/example/meet09/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/meet09/data/dao/MahasiswaDao.kt
@@ -13,15 +13,15 @@ interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
 
-    @Update
-    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
-
-    @Delete
-    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
-
     @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
     fun getAllMahasiswa(): Flow<List<Mahasiswa>>
 
     @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
     fun getMahasiswa(nim: String): Flow<Mahasiswa>
+
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }

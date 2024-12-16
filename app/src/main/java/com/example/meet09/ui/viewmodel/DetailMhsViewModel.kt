707c99f8fde4b837400046dbc83227c1edd99887
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-12-16 13:51:39 +0700

    membuat data class DetailUiState di DetailMhsViewModel.kt

--- app/src/main/java/com/example/meet09/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/meet09/ui/viewmodel/DetailMhsViewModel.kt
@@ -2,6 +2,21 @@ package com.example.meet09.ui.viewmodel
 
 import com.example.meet09.data.entity.Mahasiswa
 
+//data class untuk menampung data yang akan ditampilkan ke UI
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty : Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
+//menambahkan data dari entity ke UI
 fun Mahasiswa.toDetailUiEvent () : MahasiswaEvent{
     return MahasiswaEvent(
         nim = nim,

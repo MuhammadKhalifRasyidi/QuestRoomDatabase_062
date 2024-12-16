e4b3318ea4a2068043c17b9a65ce181211c997e0
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-12-16 13:49:17 +0700

    membuat function Mahasiswa di DetailMhsViewModel.kt

--- app/src/main/java/com/example/meet09/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/meet09/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,2 +1,14 @@
 package com.example.meet09.ui.viewmodel
 
+import com.example.meet09.data.entity.Mahasiswa
+
+fun Mahasiswa.toDetailUiEvent () : MahasiswaEvent{
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}

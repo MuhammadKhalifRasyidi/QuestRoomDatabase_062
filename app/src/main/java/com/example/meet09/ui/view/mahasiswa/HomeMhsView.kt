bb7f848459c0fe2805e65768502ce44acf32db9d
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-12-16 14:42:58 +0700

    fix menambahkan function pada HomeMhsView.kt fun HomeMhsView serta isinya yang di tambahkan

--- app/src/main/java/com/example/meet09/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/meet09/ui/view/mahasiswa/HomeMhsView.kt
@@ -25,6 +25,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
